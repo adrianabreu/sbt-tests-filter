c73e5c28ec6c0c7af683c9e4339f414565bb1e12
Adrian Abreu <aabreu@countergrogwise.com>
2022-02-07 21:24:28 +0100

    passing tests

---
+++ src/test/scala/example/HelloSpec.scala
@@ -0,0 +1,20 @@
+package example
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.Tag
+  
+object MyTag extends Tag("MyTag")
+  
+class ExampleSpec extends AnyFlatSpec {
+
+  it must "add correctly" in {
+    val sum = 4 + 1
+    assert(sum === 5)
+  }
+  
+  it must "subtract correctly" taggedAs(MyTag) in {
+    val diff = 4 - 1
+    assert(diff === 3)
+  }
+}
